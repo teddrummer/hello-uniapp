eccb1ed1c087df6cc5a41982ee2dba1232051504
18082227 <2wsx!QAZ>
2019-12-12 14:13:32 +0800

    刚刚开始

---
+++ pages/list/list.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		<view class="uni-flex uni-row">
+			<view class="flex-item uni-bg-red">A</view>
+			<view class="flex-item uni-bg-green">B</view>
+			<view class="flex-item uni-bg-blue">C</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
